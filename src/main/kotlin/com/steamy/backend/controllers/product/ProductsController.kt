a792519b9c7e51936a8f75989608c8b77dc93785
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-07-05 19:26:17 +0800

    implemented dynamic query conditions, refactored controller for brevity

--- src/main/kotlin/com/steamy/backend/controllers/product/ProductsController.kt
+++ src/main/kotlin/com/steamy/backend/controllers/product/ProductsController.kt
@@ -1,9 +1,9 @@
 package com.steamy.backend.controllers.product
 
+import com.steamy.backend.controllers.product.component.ProductsSummaryComponent
 import com.steamy.backend.controllers.product.model.Products
 import com.steamy.backend.controllers.product.model.ProductsSummary
 import com.steamy.backend.controllers.product.repository.ProductsRepository
-import com.steamy.backend.controllers.product.repository.ProductsSummaryRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.repository.findByIdOrNull
@@ -16,7 +16,7 @@ import java.util.*
 @RequestMapping( "/product" )
 class ProductsController(
     val productsRepository: ProductsRepository,
-    val productsSummaryRepository: ProductsSummaryRepository ) {
+    val productsSummaryComponent: ProductsSummaryComponent ) {
 
     @GetMapping( value = ["/{id}"], produces = ["application/json"] )
     fun getProduct( @PathVariable( "id" ) product_id: Int ): ResponseEntity<Any> {
@@ -29,22 +29,14 @@ class ProductsController(
 
     @GetMapping( value = ["/list"], produces = ["application/json"] )
     fun getProductList( @RequestParam( "name", required = false ) name: String?,
-                        @RequestParam( "category", required = false ) category: String?,
+                        @RequestParam( "price", required = false ) price: Double?,
                         @RequestParam( "sort", required = false ) sort: String?,
                         @RequestParam( "pageNo", required = false, defaultValue = "0" ) page_no: Int,
-                        @RequestParam( "pageSize", required = false, defaultValue = "10" ) page_size: Int ): ResponseEntity<Any> {
-        val product_list: Page<ProductsSummary> =
-            if ( !name.isNullOrBlank() && !category.isNullOrBlank() )
-                this.productsSummaryRepository.findAllByProdNameContainingAndProdCategory( name, category, PageRequest.of( page_no, page_size ) );
-            else if ( !name.isNullOrBlank() )
-                this.productsSummaryRepository.findAllByProdNameContaining( name, PageRequest.of( page_no, page_size ) );
-            else if ( !category.isNullOrBlank() )
-                this.productsSummaryRepository.findAllByProdCategory( category, PageRequest.of( page_no, page_size ) );
-            else this.productsSummaryRepository.findAll( PageRequest.of( page_no, page_size ) );
+                        @RequestParam( "pageSize", required = false, defaultValue = "9" ) page_size: Int ): ResponseEntity<Any> {
 
-        if ( product_list.isEmpty ) return ResponseEntity( "You must specify a product's name, or its category and/or price range", HttpStatus.NOT_FOUND )
-        product_list.forEach { it?.prodImage = Base64.getEncoder().encodeToString( it.prodImage?.toByteArray() ) }
+        val product_list: Page<ProductsSummary> = this.productsSummaryComponent.findAll( name, price, PageRequest.of( page_no, page_size ) );
 
-        return ResponseEntity( product_list, HttpStatus.FOUND )
+        if ( product_list.isEmpty ) return ResponseEntity( "No products found", HttpStatus.NOT_FOUND );
+        return ResponseEntity( product_list, HttpStatus.FOUND );
     }
 }
---
+++ src/main/kotlin/com/steamy/backend/controllers/product/component/ProductsSummaryComponent.kt
@@ -0,0 +1,38 @@
+package com.steamy.backend.controllers.product.component
+
+import com.steamy.backend.controllers.product.model.ProductsSummary
+import com.steamy.backend.controllers.product.repository.ProductsSummaryRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.domain.Specification
+import org.springframework.stereotype.Component
+
+@Component
+class ProductsSummaryComponent( val productsSummaryRepository: ProductsSummaryRepository ) {
+
+    fun findAll( name: String?, price: Double?, pageable: Pageable): Page<ProductsSummary> {
+        var specification: Specification<ProductsSummary> = Specification.where( null );
+        name?.let { specification = specification.and( byName( name ) ) };
+        price?.let { specification = specification.and( byPrice( price ) ) };
+
+        return this.productsSummaryRepository.findAll( specification, pageable );
+    };
+
+    private fun byName( name: String ): Specification<ProductsSummary> {
+        return Specification<ProductsSummary> { root, _, builder ->
+            builder.like( root.get( "prodName" ), "%$name%" );
+        }
+    }
+
+    private fun byCategory( category: String ): Specification<ProductsSummary> {
+        return Specification<ProductsSummary> { root, _, builder ->
+            builder.like( root.get( "prodCategory" ), category );
+        }
+    }
+
+    private fun byPrice( price: Double ): Specification<ProductsSummary> {
+        return Specification<ProductsSummary> { root, _, builder ->
+            builder.lessThanOrEqualTo( root.get( "prodPrice" ), price );
+        }
+    }
+}
